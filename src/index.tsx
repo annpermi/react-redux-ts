7af2c6b275c5a5e8099cbaaf6f6bbff46f8eba27
annpermi <perminova.anna85@gmail.com>
2022-01-27 17:26:55 -0500

    functional component

--- src/index.tsx
+++ src/index.tsx
@@ -2,38 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 interface AppProps {
-  color?: string;
+  color: string;
 }
 
-interface AppState {
-  counter: number;
-}
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>;
+};
 
-class App extends React.Component<AppProps, AppState> {
-  // state = { counter: 0 };
+// class App extends React.Component<AppProps, AppState> {
+//   // state = { counter: 0 };
 
-  constructor(props: AppProps) {
-    super(props);
-    this.state = { counter: 0 };
-  }
+//   constructor(props: AppProps) {
+//     super(props);
+//     this.state = { counter: 0 };
+//   }
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
 
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        <p>{this.state.counter}</p>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         <p>{this.state.counter}</p>
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(<App color="red" />, document.querySelector("#root"));
