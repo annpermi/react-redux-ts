3ee606eb5be8fcb35c4eea6188d2d3249d129239
annpermi <perminova.anna85@gmail.com>
2022-02-05 16:56:29 -0500

    fetchTodos added interfaces

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,28 @@
 import axios from "axios";
 import { Dispatch } from "redux";
+import { ActionTypes } from "./types";
+
+// typescript check we get correct types
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
 
 const url = "https://jsonplaceholder.typicode.com/todos";
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
+    //<Todo[]>  --> we will get array of objects
+    const response = await axios.get<Todo[]>(url);
 
-    dispatch({
-      type: "FETCH_TODOS",
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
       payload: response.data,
     });
   };
